66eb0afab761e41eed791664f12e6f09dae1c96f
Daan van Rooten <d.vanrooten@gmail.com>
2024-11-04 10:22:08 +0100

    Oefenen

--- assignment-3/script.js
+++ assignment-3/script.js
@@ -1,12 +1,18 @@
 // Sla de buttons in variabelen op.
 const redButton = document.getElementById('redButton');
-const greenButton = document.getElementById('?');
-const blueButton = document.getElementById('?');
+const greenButton = document.getElementById('greenButton');
+const blueButton = document.getElementById('blueButton');
 
 // sla de tekst op in een variabele.
-const text1 = document.getElementById('text1');
+const text1 = document.querySelector('.text-elements');
+
 
 redButton.onclick = () => {ChangeColor(text1, 'red')};
+greenButton.onclick = () => {ChangeColor(text1, 'green')};
+blueButton.onclick = () => {ChangeColor(text1, 'blue')};
+
+
+
 // greenButton.onclick = () => { ? };
 // blueButton.onclick = () => { ? };
 
@@ -17,6 +23,7 @@ redButton.onclick = () => {ChangeColor(text1, 'red')};
 function ChangeColor(element, color) {
     element.style.color = color;
 }
+
 
 /* Extra moeilijkheidsgraad opdracht 3:
 
--- assignment-4/script.js
+++ assignment-4/script.js
@@ -1,13 +1,14 @@
 // Verzin een willekeurig nummer tussen 1 en 10
 let randomNumber = GenerateRandomNumber();
+console.log(randomNumber)
 
 // Grab elements from the DOM
 const userGuessInput = document.getElementById('userGuess');
 const feedback = document.getElementById('feedback');
 
 // Hoe kunnen we de guessButton en playAgainButton selecteren?
-const guessButton = document.getElementById('?');
-const playAgainButton = document.getElementById('?');
+const guessButton = document.getElementById('guessButton');
+const playAgainButton = document.getElementById('playAgainButton');
 
 // Function to handle guesses
 guessButton.onclick = () => {
@@ -18,12 +19,27 @@ guessButton.onclick = () => {
     }
 
     // zet de input om naar een getal
-    const userGuess = '?';
+    const userGuess = parseInt(userGuessInput.value);
     
 
     // vergelijk het getal met het random getal welke 3 dingen moeten we checken?
     // Hint: is het getal te hoog, te laag of gelijk?
+    if(userGuess=== randomNumber){
+        // nu heb je gewonnen
+        feedback.textContent 'you won';
+        playAgainButton.style.display = 'inline'
+        guessButton.style.display = ''
 
+    }
+
+    else if (userGuess < randomNumber){
+        // te laag
+        feedback.textContent= 'to low';
+    }
+    else if (userGuess > randomNumber){
+        //te hoog
+         feedback.textContent= 'to high';
+    }
         
     // dit toont de play again button en verbergt de guess button
         // guessButton.style.display = 'none';
@@ -33,7 +49,7 @@ guessButton.onclick = () => {
 // Function to reset the game
 playAgainButton.onclick = () => {
     randomNumber = GenerateRandomNumber();
-    feedback.textContent = '';
+    feedback.textContent = 'playAgainButton';
     userGuessInput.value = '';
     guessButton.style.display = 'inline';
     playAgainButton.style.display = 'none';
